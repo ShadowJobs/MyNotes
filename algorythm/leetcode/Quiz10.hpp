18bc4d7d80ea73852d11d6008f1b0d9ce3982bd0
linying <linying@kuaishou.com>
2019-12-23 10:27:59 +0800

    leetcode quizs

---
+++ algorythm/leetcode/Quiz10.hpp
@@ -0,0 +1,17 @@
+//
+//  Quiz10.hpp
+//  LeetCode
+//
+//  Created by 林影 on 2019/12/18.
+//
+
+#ifndef Quiz10_hpp
+#define Quiz10_hpp
+#include <string>
+using namespace std;
+class Quiz10 {
+public:
+    bool isMatch(string s, string p);
+    void execute();
+};
+#endif /* Quiz10_hpp */
